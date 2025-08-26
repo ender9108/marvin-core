946ef5ce046b77b45fae560e7856060565a09657
Alexandre BERTHELOT <berthelot@wcentric.com>
2025-08-26 12:21:13 +0200

    Create MQTT Service

--- config/services/system.php
+++ config/services/system.php
@@ -8,6 +8,7 @@ use App\System\Domain\Repository\UserTypeRepositoryInterface;
 use App\System\Infrastructure\Doctrine\Repository\DoctrineUserRepository;
 use App\System\Infrastructure\Doctrine\Repository\DoctrineUserStatusRepository;
 use App\System\Infrastructure\Doctrine\Repository\DoctrineUserTypeRepository;
+use App\System\Infrastructure\Symfony\Service\MqttService;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
@@ -25,6 +26,24 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         ->defaults()
         ->autowire()
         ->autoconfigure()
+    ;
+
+    $services->set(MqttService::class)
+        ->args([
+            '%env(string:MQTT_HOST)%',
+            '%env(int:MQTT_PORT)%',
+            'marvin-core-mqtt',
+            '%env(default::null:MQTT_USER)%',
+            '%env(default::null:MQTT_PASSWORD)%',
+            '%env(default::5:MQTT_PROTOCOL_LEVEL)%',
+            '%env(bool:default::false:MQTT_TLS)%',
+            '%env(MQTT_CERT_FILE)%)',
+            '%env(MQTT_KEY_FILE)%)',
+            '%env(MQTT_CA_FILE)%)',
+            '%env(bool:default::true:MQTT_VERIFY_PEER)%)',
+            '%env(bool:default::false:MQTT_ALLOW_SELF_SIGNED)%)',
+        ])
+        //->public()
     ;
 
     $services->load('App\\System\\Infrastructure\\DataFixtures\\', dirname(__DIR__, 2) . '/src/System/Infrastructure/DataFixtures/');
