ba04350f4d6a9e727d74ab23b34be521c5e953cb
Alexandre BERTHELOT <berthelot@wcentric.com>
2025-08-26 09:26:56 +0200

    Clean code and bugfix marvin manager command binding déclaration with enum object

--- src/System/Infrastructure/ApiPlatform/Resource/UserStatusResource.php
+++ src/System/Infrastructure/ApiPlatform/Resource/UserStatusResource.php
@@ -12,10 +12,8 @@ use EnderLab\BlameableBundle\Trait\ApiPlatform\ResourceBlameableTrait;
 use EnderLab\DddCqrsBundle\Infrastructure\ApiPlatform\ApiResourceInterface;
 use EnderLab\DddCqrsBundle\Infrastructure\ApiPlatform\State\Provider\EntityToApiStateProvider;
 use EnderLab\TimestampableBundle\Trait\ApiPlatform\ResourceTimestampableTrait;
-use Symfony\Component\JsonStreamer\Attribute\JsonStreamable;
 use Symfony\Component\Validator\Constraints as Assert;
 
-#[JsonStreamable]
 #[ApiResource(
     shortName: 'user_status',
     operations: [
