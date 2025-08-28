38decd44098e3f093193d122f36551b9211ed3d6
Alexandre BERTHELOT <berthelot@wcentric.com>
2025-08-28 14:29:53 +0200

    Update zigbee2mqtt bundle => Update event handler attribut

--- libs/enderlab/marvin-zigbee2mqtt-bundle/src/Application/Command/Plugin/EnablePluginCommandHandler.php
+++ libs/enderlab/marvin-zigbee2mqtt-bundle/src/Application/Command/Plugin/EnablePluginCommandHandler.php
@@ -2,13 +2,13 @@
 
 namespace EnderLab\Zigbee2mqttBundle\Application\Command\Plugin;
 
-use App\System\Application\Command\Plugin\EnablePluginCommand;
-use EnderLab\DddCqrsBundle\Application\Command\Attribute\AsCommandHandler;
+use App\System\Domain\Event\Plugin\PluginEnabled;
+use EnderLab\DddCqrsBundle\Domain\Event\Attribute\AsDomainEventHandler;
 
-#[AsCommandHandler]
+#[AsDomainEventHandler(['$.system.plugin.enabled',])]
 class EnablePluginCommandHandler
 {
-    public function __invoke(EnablePluginCommand $command): void
+    public function __invoke(PluginEnabled $event): void
     {
         /** @todo a faire quand j'aurais la liste des services à arrêter */
     }
