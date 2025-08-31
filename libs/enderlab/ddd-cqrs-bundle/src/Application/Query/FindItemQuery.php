3ce2291fbe672eca00417ac7fc201e311df60adf
Alexandre BERTHELOT <berthelot@wcentric.com>
2025-08-31 10:23:32 +0200

    Update FindItemQuery to manage uuid

--- libs/enderlab/ddd-cqrs-bundle/src/Application/Query/FindItemQuery.php
+++ libs/enderlab/ddd-cqrs-bundle/src/Application/Query/FindItemQuery.php
@@ -5,7 +5,7 @@ namespace EnderLab\DddCqrsBundle\Application\Query;
 class FindItemQuery implements QueryInterface
 {
     public function __construct(
-        public int $id,
+        public int|string $id,
         public string $className,
     ) {
     }
