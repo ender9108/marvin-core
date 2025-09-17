63a4a3b1d6b43de1421fc3af7419711b4d9d2bb7
Alexandre BERTHELOT <berthelot@wcentric.com>
2025-09-17 13:29:00 +0200

    Add github actions

--- src/Schedule.php
+++ src/Schedule.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace Marvin;
 
 use Symfony\Component\Scheduler\Attribute\AsSchedule;
 use Symfony\Component\Scheduler\Schedule as SymfonySchedule;
@@ -8,10 +8,10 @@ use Symfony\Component\Scheduler\ScheduleProviderInterface;
 use Symfony\Contracts\Cache\CacheInterface;
 
 #[AsSchedule]
-class Schedule implements ScheduleProviderInterface
+readonly class Schedule implements ScheduleProviderInterface
 {
     public function __construct(
-        private readonly CacheInterface $cache,
+        private CacheInterface $cache,
     ) {
     }
 
