778e8c8953f2dfc70d13720b76c9ee732c42c815
Alexandre BERTHELOT <berthelot@wcentric.com>
2025-10-07 16:39:49 +0200

    Update bounded context domotic

--- src/Domotic/Domain/Model/Zone.php
+++ src/Domotic/Domain/Model/Zone.php
@@ -3,6 +3,8 @@
 namespace Marvin\Domotic\Domain\Model;
 
 use DateTimeImmutable;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Marvin\Domotic\Domain\ValueObject\Area;
 use Marvin\Domotic\Domain\ValueObject\Identity\ZoneId;
 use Marvin\Shared\Domain\ValueObject\CreatedAt;
@@ -13,6 +15,9 @@ final class Zone
 {
     public readonly ZoneId $id;
 
+    /** @var Collection<int, Device> */
+    public private(set) Collection $devices;
+
     public function __construct(
         private(set) Label $label,
         private(set) Area $area,
@@ -21,5 +26,43 @@ final class Zone
         public readonly CreatedAt $createdAt = new CreatedAt(new DateTimeImmutable())
     ) {
         $this->id = new ZoneId();
+        $this->devices = new ArrayCollection();
+    }
+
+    public function update(
+        ?Label $label = null,
+        ?Area $area = null,
+        ?Zone $parentZone = null
+    ): void {
+        $this->label = $label ?? $this->label;
+        $this->area = $area ?? $this->area;
+        $this->parentZone = $parentZone ?? $this->parentZone;
+    }
+
+    public function remove(): void
+    {
+        foreach ($this->devices as $device) {
+            $this->removeDevice($device);
+        }
+    }
+
+    public function addDevice(Device $device): Zone
+    {
+        if (!$this->devices->contains($device)) {
+            $this->devices->add($device);
+            $device->setZone($this);
+        }
+
+        return $this;
+    }
+
+    public function removeDevice(Device $device): Zone
+    {
+        if ($this->devices->contains($device)) {
+            $this->devices->removeElement($device);
+            $device->setZone(null);
+        }
+
+        return $this;
     }
 }
