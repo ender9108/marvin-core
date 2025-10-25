94d2aca2580916b165a6ad5f7366027dd5d92ede
Alexandre BERTHELOT <berthelot@wcentric.com>
2025-10-25 10:57:43 +0200

    Update system context

--- src/System/Domain/Event/ContainerActionCompleted.php
+++ src/System/Domain/Event/ContainerActionCompleted.php
@@ -2,10 +2,11 @@
 
 namespace Marvin\System\Domain\Event;
 
+use EnderLab\DddCqrsBundle\Domain\Event\AbstractDomainEvent;
 use Marvin\Shared\Domain\ValueObject\Identity\UniqId;
 use Marvin\System\Domain\ValueObject\Identity\ContainerId;
 
-class ContainerActionCompleted
+final readonly class ContainerActionCompleted extends AbstractDomainEvent
 {
     public function __construct(
         public UniqId $correlationId,
@@ -15,5 +16,18 @@ class ContainerActionCompleted
         public ?string $output = null,
         public ?string $error = null,
     ) {
+        parent::__construct();
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'correlation_id' => $this->correlationId->toString(),
+            'container_id' => $this->containerId->toString(),
+            'action' => $this->action,
+            'success' => $this->success,
+            'output' => $this->output,
+            'error' => $this->error,
+        ];
     }
 }
--- src/System/Domain/Event/ContainerSynced.php
+++ src/System/Domain/Event/ContainerSynced.php
@@ -2,11 +2,12 @@
 
 namespace Marvin\System\Domain\Event;
 
+use EnderLab\DddCqrsBundle\Domain\Event\AbstractDomainEvent;
 use Marvin\Shared\Domain\ValueObject\Identity\UniqId;
 use Marvin\System\Domain\ValueObject\ActionStatus;
 use Marvin\System\Domain\ValueObject\Identity\ContainerId;
 
-class ContainerSynced
+final readonly class ContainerSynced extends AbstractDomainEvent
 {
     public function __construct(
         public UniqId $correlationId,
@@ -14,5 +15,16 @@ class ContainerSynced
         public ActionStatus $status,
         public array $details = [],
     ) {
+        parent::__construct();
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'correlation_id' => $this->correlationId->toString(),
+            'container_id' => $this->containerId->toString(),
+            'status' => $this->status->value,
+            'details' => $this->details,
+        ];
     }
 }
